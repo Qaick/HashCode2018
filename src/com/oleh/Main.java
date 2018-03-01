fda91a33ea32fe79f21c7f1b402df14afd58017d
Oleh Kapustianov <ole@seekingalpha.com>
2018-03-01 23:07:33 +0200

    Submit #2

--- src/com/oleh/Main.java
+++ src/com/oleh/Main.java
@@ -5,9 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -46,26 +44,67 @@ public class Main {
         // Solve of the problem:
 
         List<List<Integer>> result = new ArrayList<>();
+
+//        for (int i = 0; i < vehicles; i++) {
+//            result.add(new ArrayList<Integer>());
+//        }
+//
+//        int ridesCount = ridesArr.length;
+//
+//        for (int i = 0; i < ridesCount; i++) {
+//            if (i < result.size()) {
+//                result.get(i).add(i);
+//            } else {
+//                result.get(i % result.size()).add(i);
+//            }
+//        }
+//
+//        for (int i = 0; i < result.size(); i++) {
+//            List<Integer> arrayList = result.get(i);
+//            arrayList.add(0, arrayList.size());
+//        }
+
+        // Solve of the problem:
+
+        // key = vehicleId, value = List<RideId>
+        Map<Integer, ArrayList<Integer>> resMap = new HashMap<>();
+
+        ArrayList<Vehicle> vehiclesPull = new ArrayList<>();
         for (int i = 0; i < vehicles; i++) {
-            result.add(new ArrayList<Integer>());
+            vehiclesPull.add(new Vehicle());
         }
 
-        int ridesCount = ridesArr.length;
+        ArrayList<Ride> ridesPull = new ArrayList<>();
+        for (int i = 0; i < rides; i++) {
+            ridesPull.add(new Ride(ridesArr[i][0], ridesArr[i][1], ridesArr[i][2], ridesArr[i][3]));
+        }
 
-        for (int i = 0; i < ridesCount; i++) {
-            if (i < result.size()) {
-                result.get(i).add(i);
+        int rideId = 0;
+
+        while (!ridesPull.isEmpty()) {
+            Ride ride = ridesPull.remove(0);
+
+            int vehicleId = findNearestFreeVehicle(vehiclesPull, ride);
+            Vehicle vehicle = vehiclesPull.get(vehicleId);
+            vehicle.busy = true;
+
+            if (resMap.containsKey(vehicleId)) {
+                resMap.get(vehicleId).add(rideId);
             } else {
-                result.get(i % result.size()).add(i);
+                resMap.put(vehicleId, new ArrayList<>(Collections.singleton(rideId)));
             }
+
+            rideId++;
         }
+
+        result.clear();
+        result.addAll(resMap.values());
 
         for (int i = 0; i < result.size(); i++) {
             List<Integer> arrayList = result.get(i);
             arrayList.add(0, arrayList.size());
         }
-//        System.out.println("ridesArr = " + ridesArr);
-//        System.out.println("result = " + result);
+
         System.out.println("solved "+file.getName());
         return result;
     }
@@ -81,5 +120,54 @@ public class Main {
         }
         Path file = Paths.get(filename);
         Files.write(file, lines, Charset.forName("UTF-8"));
+    }
+
+    static int findNearestFreeVehicle(ArrayList<Vehicle> vehicles, Ride ride) {
+        int vehiclePos = 0;
+        int minDis = Integer.MAX_VALUE;
+        for (int i = 0; i < vehicles.size(); i++) {
+            Vehicle v = vehicles.get(i);
+            int distance;
+            if (v.busy) {
+                distance = getDistance(v.coor, ride.finish);
+            } else {
+                distance = getDistance(v.coor, ride.start);
+            }
+            if (distance < minDis) {
+                minDis = distance;
+                vehiclePos = i;
+            }
+        }
+        return vehiclePos;
+    }
+
+    static int getDistance(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    }
+}
+
+class Vehicle {
+
+    boolean busy;
+
+    int[] coor = new int[2];
+
+    Vehicle() {
+        busy = false;
+        coor[0] = 0;
+        coor[1] = 0;
+    }
+}
+
+class Ride {
+
+    int[] start = new int[2];
+    int[] finish = new int[2];
+
+    Ride(int a, int b, int x, int y) {
+        start[0] = a;
+        start[1] = b;
+        finish[0] = x;
+        finish[1] = y;
     }
 }
