082a9ffb41e9988b1ac6a54f0fd44ba73d50ec79
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2018-03-01 20:44:47 +0200

    input data

--- src/com/oleh/Main.java
+++ src/com/oleh/Main.java
@@ -1,8 +1,32 @@
 package com.oleh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int rows = in.nextInt();
+        int columns = in.nextInt();
+        int vehicles = in.nextInt();
+        int rides = in.nextInt();
+        int bonus = in.nextInt();
+        int steps = in.nextInt();
+
+        int[][] ridesArr = new int[rows][6];
+        
+        for (int i = 0; i < rows; i++)
+        {
+            ridesArr[i] = new int[]{
+                    in.nextInt(), // row start intersection
+                    in.nextInt(), // column start intersection
+                    in.nextInt(), // row finish intersection
+                    in.nextInt(), // column finish intersection
+                    in.nextInt(), // earliest start
+                    in.nextInt()  // latest start
+            };
+        }
+        
+        
     }
 }
