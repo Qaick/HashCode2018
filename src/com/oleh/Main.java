bf3ad663ab0654999b7d2529f57e35d26d7d9709
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2018-03-01 20:50:41 +0200

    reading files

--- src/com/oleh/Main.java
+++ src/com/oleh/Main.java
@@ -1,11 +1,18 @@
 package com.oleh;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        File a_file = new File("a_example.in");
+        File b_file = new File("b_should_be_easy.in");
+        File c_file = new File("c_no_hurry.in");
+        File d_file = new File("d_metropolis.in");
+        File e_file = new File("e_high_bonus.in");
+        
+        Scanner in = new Scanner(a_file);
         int rows = in.nextInt();
         int columns = in.nextInt();
         int vehicles = in.nextInt();
@@ -26,7 +33,10 @@ public class Main {
                     in.nextInt()  // latest start
             };
         }
+
+        // Solve of the problem:
         
         
+        System.out.println("ridesArr = " + ridesArr);
     }
 }
