753d8410174789bac5142236b862b5871ffbb5f8
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-04-24 00:14:44 +0300

    Tests values and duration.

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,30 +1,40 @@
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
 
 /**
  * Все числа парные кроме единицы.
  * Есть критерий в том что все числа которым надо количество множителей больше 4-х должны делиться на 1 2 3 4
  */
 public class Main2 {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        printTestCSV();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        find(n);
+        System.out.println(find(n));
     }
 
-    static void find(int n) throws InterruptedException {
-        System.out.print(n + " :");
+    static int find(int n) {
+//        System.out.print(n + " :");
         if (n == 1)
-            System.out.println(1);
+            return 1;
         else {
             for (int i = 4; i < Integer.MAX_VALUE - 1; i+=2) {
-                Thread.sleep(0);
+                sleep();
 //                System.out.println("      "+i);
                 if (mult(i, n)) {
-                    System.out.println("\n"+i);
-                    break;
+                    return i;
                 }
             }
         }
+        throw new RuntimeException();
     }
 
     static boolean mult(int n, int find) {
@@ -37,8 +47,41 @@ public class Main2 {
                 s.append(" ").append(i);
 //                System.out.println(i+" * "+ n/i);
             }
-        if (ans == find)
-            System.out.print(s);
+//        if (ans == find)
+//            System.out.print(s);
         return ans == find;
+    }
+
+    static void printTestCSV() {
+        for (int i = 1; i <= 50; i++) {
+            System.out.print("\""+i+","+findFast(i, 2)+"\",");
+        }
+    }
+
+    static int findFast(int n, int timeout) {
+        RunnableFuture<Integer> future = new FutureTask<>(() -> find(n));
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(future);
+        try
+        {
+            return future.get(timeout, TimeUnit.SECONDS);    // wait 1 second
+        }
+        catch (TimeoutException | InterruptedException | ExecutionException ex)
+        {
+            // timed out. Try to stop the code if possible.
+            future.cancel(true); // Thread.interrupt
+        }
+        finally {
+            service.shutdown();
+        }
+        return -1;
+    }
+
+    static void sleep() {
+        try {
+            Thread.sleep(0);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
@@ -16,14 +17,18 @@ class MainTest {
             ,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50})
     void name(int n) {
         assertTimeoutPreemptively(Duration.ofMillis(2000), () -> Main.findMatching(n));
-        // I want to see the number, and the divisors:
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34
             ,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50})
-    void name2(int n) {
+    void test_duration(int n) {
         assertTimeoutPreemptively(Duration.ofMillis(2000), () -> Main2.find(n));
-        // I want to see the number, and the divisors:
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,4", "3,12", "4,24", "5,36", "6,60", "7,192", "8,120", "9,180", "10,240", "11,576", "12,360", "13,1296", "14,900", "15,720", "16,840", "17,9216", "18,1260", "19,786432", "20,1680", "21,2880", "22,15360", "23,3600", "24,2520", "25,6480", "26,61440", "27,6300", "28,6720", "29,-1", "30,5040", "31,-1", "32,7560", "33,46080", "34,983040", "35,25920", "36,10080", "37,-1", "38,32400", "39,184320", "40,15120", "41,44100", "42,20160", "43,-1", "44,107520", "45,25200", "46,-1", "47,-1", "48,27720", "49,233280", "50,45360"})
+    void test_values_duration(int n, int exp) {
+        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> assertEquals(exp, Main2.find(n)));
     }
 }
