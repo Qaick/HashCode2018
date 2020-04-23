5404e3d78d153020d8223a8ca99a62a646797780
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-04-23 23:36:46 +0300

    only even numbers: 8 failed -> 6

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/**
+ * Все числа парные кроме единицы.
+ * Есть критерий в том что все числа которым надо количество множителей больше 4-х должны делиться на 1 2 3 4
+ */
 public class Main2 {
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
@@ -8,14 +12,15 @@ public class Main2 {
     }
 
     static void find(int n) throws InterruptedException {
+        System.out.print(n + " :");
         if (n == 1)
             System.out.println(1);
         else {
-            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            for (int i = 4; i < Integer.MAX_VALUE - 1; i+=2) {
                 Thread.sleep(0);
 //                System.out.println("      "+i);
                 if (mult(i, n)) {
-                    System.out.println(i);
+                    System.out.println("\n"+i);
                     break;
                 }
             }
@@ -25,11 +30,15 @@ public class Main2 {
     static boolean mult(int n, int find) {
         int ans = 1;
         int lim = (int) Math.sqrt(n);
+        StringBuilder s = new StringBuilder(" 1");
         for (int i = 2; i <= lim; i++)
             if (n % i == 0) {
                 ans++;
+                s.append(" ").append(i);
 //                System.out.println(i+" * "+ n/i);
             }
+        if (ans == find)
+            System.out.print(s);
         return ans == find;
     }
 }
