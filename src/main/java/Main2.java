ad5f5365928aa8fa677bd36a07e870d07b62e549
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-04-27 19:13:04 +0300

    Used numbers theory algorithm. Extremely fast. See image.

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ExecutionException;
@@ -16,9 +21,9 @@ public class Main2 {
 
     public static void main(String[] args) {
 //        printTestCSV();
-//        Scanner in = new Scanner(System.in);
-//        int k = in.nextInt();
-        System.out.println(find(31));
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        System.out.println(findFastest(k));
     }
 
     static int find(int k) {
@@ -30,7 +35,7 @@ public class Main2 {
         else if (k == 3)
             return 12;
         else {
-            for (int i = 24; i < Integer.MAX_VALUE - 3; i+=12) {
+            for (int i = 24; i < Integer.MAX_VALUE - 3; i += 12) {
                 sleep(i);
 //                System.out.print(i+":");
                 if (suggest(i, k)) {
@@ -48,6 +53,7 @@ public class Main2 {
         Set<Integer> subset = new TreeSet();
         // TODO I would like to see the set of all the dividers and maybe I could fill up the gaps or find a pattern
         for (int i = 5; i <= lim; i++) // TODO it looks like here I have only combination of some numbers, a few primes
+            // TODO right now I don't think it will be resultative
             if (suggestedN % i == 0) {
                 subset.add(i);
                 currentK++;
@@ -64,7 +70,7 @@ public class Main2 {
 
     private static void printTestCSV() {
         for (int i = 1; i <= 50; i++) {
-            System.out.print("\""+i+","+findFast(i, 2)+"\",");
+            System.out.print("\"" + i + "," + findInterruptible(i, 2) + "\",");
         }
         System.out.println("\n\n");
         System.out.println("set = " + set);
@@ -72,18 +78,16 @@ public class Main2 {
 
     private static Set<Integer> set = new TreeSet<>();
 
-    private static int findFast(int k, int timeout) {
+    private static int findInterruptible(int k, int timeout) {
         RunnableFuture<Integer> future = new FutureTask<>(() -> find(k));
         ExecutorService service = Executors.newSingleThreadExecutor();
         service.execute(future);
-        try{
+        try {
             return future.get(timeout, TimeUnit.SECONDS);    // wait 1 second
-        }
-        catch (TimeoutException | InterruptedException | ExecutionException ex){
+        } catch (TimeoutException | InterruptedException | ExecutionException ex) {
             // timed out. Try to stop the code if possible.
             future.cancel(true); // Thread.interrupt
-        }
-        finally {
+        } finally {
             service.shutdown();
         }
         return -1;
@@ -93,7 +97,7 @@ public class Main2 {
         try {
             Thread.sleep(0);
         } catch (InterruptedException e) {
-            System.out.println("last:"+last);
+            System.out.println("last:" + last);
             throw new RuntimeException(e);
         }
     }
@@ -104,5 +108,81 @@ public class Main2 {
      */
     private static void log(Object s) {
         System.out.print(s);
+    }
+
+    /**
+     * @see src/main/resources/number with minimum dividers.PNG
+     */
+    static long findFastest(int k) {
+        List<Long> numbers = numbersWDividers(k);
+        long min = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            min = Math.min(numbers.get(i), min);
+        }
+        return min;
+    }
+
+    /**
+     * Returns number representation of multiplication prime numbers.
+     */
+    private static List<Integer> factorize(int x) {
+        List<Integer> factors = new ArrayList<>();
+        // TODO use prime numbers instead of i++
+        for (int i = 2; i <= Math.sqrt(x); i++) { // might be problems with rounding of sqrt
+            while (x % i == 0) {
+                factors.add(0, i);
+                x /= i;
+            }
+        }
+        if (x != 1) {
+            factors.add(0, x);
+        }
+        return factors;
+    }
+
+    private static final int[] PRIMES;
+
+    static {
+        // primes before 100
+        PRIMES = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    }
+
+    private static Map<Integer, Integer> getCalculable(List<Integer> numbers) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            result.put(PRIMES[i], numbers.get(i) - 1);
+        }
+        return result;
+    }
+
+    private static List<Long> numbersWDividers(int k) {
+        List<Long> longs = new ArrayList<>();
+        longs.addAll(numberWithDividers(k * 2 - 1));
+        longs.addAll(numberWithDividers(k*2));
+        return longs;
+    }
+
+    private static List<Long> numberWithDividers(int k) {
+        List<List<Integer>> allFactors = getFactorsList(factorize(k));
+        List<Long> results = new ArrayList<>();
+        for (List<Integer> factor : allFactors) {
+            Map<Integer, Integer> calculable = getCalculable(factor);
+            long result = 1;
+            for (Map.Entry<Integer, Integer> entry : calculable.entrySet()) {
+                result *= Math.pow(entry.getKey(), entry.getValue());
+            }
+            results.add(result);
+        }
+        return results;
+    }
+
+    private static List<List<Integer>> getFactorsList(List<Integer> factors) {
+        ArrayList<List<Integer>> lists = new ArrayList<>();
+        lists.add(factors);
+        return lists;
+    }
+
+    private static boolean isPrime(int n) {
+        return false;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -22,23 +22,19 @@ class MainTest {
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34
             ,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50})
-    void test_duration(int n) {
-        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> Main2.find(n));
+    void test_duration(int k) {
+        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> Main2.find(k));
     }
 
-    // s[31]:= 3221225472
-    // s[37]:= 206158430208
-    // s[43]:= 5308416
-    // s[47]:= 9663676416
     @ParameterizedTest
-    @CsvSource({"1,1", "2,4", "3,12", "4,24", "5,36", "6,60", "7,192", "8,120", "9,180", "10,240", "11,576", "12,360", "13,1296", "14,900", "15,720", "16,840", "17,9216", "18,1260", "19,786432", "20,1680", "21,2880", "22,15360", "23,3600", "24,2520", "25,6480", "26,61440", "27,6300", "28,6720", "29,2359296", "30,5040", "31,-1", "32,7560", "33,46080", "34,983040", "35,25920", "36,10080", "37,-1", "38,32400", "39,184320", "40,15120", "41,44100", "42,20160", "43,-1", "44,107520", "45,25200", "46,2985984", "47,-1", "48,27720", "49,233280", "50,45360"})
-    void test_values_duration(int n, int exp) {
-        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> assertEquals(exp, Main2.find(n)));
+    @CsvSource({"1,1", "2,4", "3,12", "4,24", "5,36", "6,60", "7,192", "8,120", "9,180", "10,240", "11,576", "12,360", "13,1296", "14,900", "15,720", "16,840", "17,9216", "18,1260", "19,786432", "20,1680", "21,2880", "22,15360", "23,3600", "24,2520", "25,6480", "26,61440", "27,6300", "28,6720", "29,2359296", "30,5040", "31,3221225472", "32,7560", "33,46080", "34,983040", "35,25920", "36,10080", "37,206158430208", "38,32400", "39,184320", "40,15120", "41,44100", "42,20160", "43,5308416", "44,107520", "45,25200", "46,2985984", "47,9663676416", "48,27720", "49,233280", "50,45360"})
+    void test_values_duration(int k, long exp) {
+        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> assertEquals(exp, Main2.findFastest(k)));
     }
 
     @ParameterizedTest
     @CsvSource({"29,2359296", "46,2985984", "43,5308416"})
-    void test_speed_up(int n, int exp) {
-        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> assertEquals(exp, Main2.find(n)));
+    void test_speed_up(int k, int exp) {
+        assertTimeoutPreemptively(Duration.ofMillis(2000), () -> assertEquals(exp, Main2.findFastest(k)));
     }
 }
