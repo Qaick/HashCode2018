42ae33208030f5dcdb130a0a9c599899225add1c
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-04-28 16:59:50 +0300

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +11,20 @@ public class Main {
     }
 
     static void findMatching(int n) throws InterruptedException {
+    }
+
+    @Test
+    public void testBitwiseOr() {
+        Assertions.assertEquals(true, true | true);
+        Assertions.assertEquals(true, true | false);
+        Assertions.assertEquals(false, false | false);
+        boolean b = false;
+        b |= false;
+        Assertions.assertEquals(false, b);
+        b |= true;
+        Assertions.assertEquals(true, b);
+        b |= false;
+        Assertions.assertEquals(true, b);
     }
 
 }
--- src/main/java/Problem5.java
+++ src/main/java/Problem5.java
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 
