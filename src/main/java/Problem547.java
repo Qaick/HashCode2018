19aa9f3074a0808667c2f69a4589fc05f6d1511b
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-05-29 22:57:28 +0300

    c

--- src/main/java/Problem547.java
+++ src/main/java/Problem547.java
@@ -36,11 +36,11 @@ public class Problem547 {
             changes = false;
             for (int i = 1; i < m; i++) {
                 int top = map[0][i];
-                changes = isChanges(chars, changes, top, 0, i, 0, i - 1);
+                changes = isChanges(chars, changes, top, 0, i - 1, 0, i);
                 int right = map[i][m];
-                changes = isChanges(chars, changes, right, i - 1, m2, i, m2);
+                changes = isChanges(chars, changes, right, i, m2, i-1, m2);
                 int bot = map[m][i];
-                changes = isChanges(chars, changes, bot, m2, i, m2, i - 1);
+                changes = isChanges(chars, changes, bot, m2, i, m2, i-1);
                 int left = map[i][0];
                 changes = isChanges(chars, changes, left, i - 1, 0, i, 0);
             }
@@ -66,21 +66,21 @@ public class Problem547 {
     private static boolean isChanges(char[][] chars, boolean c, int side, int i1, int i2, int i3, int i4) {
         if (side != -1) {
             if (side == 0) {
-                if (chars[i1][i4] == '.' || chars[i3][i2] == '.')
+                if (chars[i1][i2] == '.' || chars[i3][i4] == '.')
                     c = true;
-                chars[i1][i4] = '\\';
-                chars[i3][i2] = '/';
+                chars[i1][i2] = '\\';
+                chars[i3][i4] = '/';
             } else if (side == 2) {
-                if (chars[i1][i4] == '.' || chars[i3][i2] == '.')
+                if (chars[i1][i2] == '.' || chars[i3][i4] == '.')
                     c = true;
-                chars[i1][i4] = '/';
-                chars[i3][i2] = '\\';
+                chars[i1][i2] = '/';
+                chars[i3][i4] = '\\';
             } else /* == 1 */ {
-                if (chars[i1][i4] == '.' && chars[i3][i2] != '.') { // хоть одна стоит
-                    chars[i1][i4] = chars[i3][i2] == '/' ? '/' : '\\';
+                if (chars[i1][i2] == '.' && chars[i3][i4] != '.') { // хоть одна стоит
+                    chars[i1][i2] = chars[i3][i4] == '/' ? '/' : '\\';
                     c = true;
-                } else if (chars[i1][i4] != '.' && chars[i3][i2] == '.') {
-                    chars[i3][i2] = chars[i1][i4] == '/' ? '/' : '\\';
+                } else if (chars[i1][i2] != '.' && chars[i3][i4] == '.') {
+                    chars[i3][i4] = chars[i1][i2] == '/' ? '/' : '\\';
                     c = true;
                 }
             }
