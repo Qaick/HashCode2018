965755d74d9e9958348d50c916aebe05752ea450
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2020-05-29 20:53:10 +0300

    c

--- src/main/java/Problem547.java
+++ src/main/java/Problem547.java
@@ -23,6 +23,67 @@ public class Problem547 {
         for (char[] aChar : chars) {
             Arrays.fill(aChar, '.');
         }
+        // check every 4 corners. If corner contain 1 or 0 I know the solve
+        if (map[0][0] !=-1) chars[0][0] = map[0][0] == 1 ? '\\' : '/';
+        if (map[0][m] !=-1) chars[0][m-1] = map[0][m] == 1 ? '/' : '\\';
+        if (map[m][0] !=-1) chars[m-1][0] = map[m][0] == 1 ? '/' : '\\';
+        if (map[m][m] !=-1) chars[m-1][m-1] = map[m][m] == 1 ? '\\' : '/';
+
+        // check 4 sides. Do it in cycle until there will be no changes?
+        boolean changes = true;
+        while(changes) {
+            changes = false;
+            for (int i = 1; i < m - 1; i++) {
+                int top = map[0][i]; // 0 1 2
+                if (top != -1) {
+                    if (top == 0) {
+                        if (chars[0][i - 1] == '.' || chars[0][i] == '.')
+                            changes = true;
+                        chars[0][i - 1] = '\\';
+                        chars[0][i] = '/';
+                    } else if (top == 2) {
+                        if (chars[0][i - 1] == '.' || chars[0][i] == '.')
+                            changes = true;
+                        chars[0][i - 1] = '/';
+                        chars[0][i] = '\\';
+                    } else /* == 1 */ {
+                        if (chars[0][i - 1] == '.' && chars[0][i] != '.') { // хоть одна стоит
+                            chars[0][i - 1] = chars[0][i] == '/' ? '/' : '\\';
+                            changes = true;
+                        } else if (chars[0][i - 1] != '.' && chars[0][i] == '.') {
+                            chars[0][i] = chars[0][i - 1] == '/' ? '/' : '\\';
+                            changes = true;
+                        }
+                    }
+                }
+                int left = map[i][0]; // 0 1 2
+                if (left != -1) {
+                    if (left == 0) {
+                        if (chars[0][i - 1] == '.' || chars[0][i] == '.')
+                            changes = true;
+                        chars[0][i - 1] = '\\';
+                        chars[0][i] = '/';
+                    } else if (left == 2) {
+                        if (chars[0][i - 1] == '.' || chars[0][i] == '.')
+                            changes = true;
+                        chars[0][i - 1] = '/';
+                        chars[0][i] = '\\';
+                    } else /* == 1 */ {
+                        if (chars[0][i - 1] == '.' && chars[0][i] != '.') { // хоть одна стоит
+                            chars[0][i - 1] = chars[0][i] == '/' ? '/' : '\\';
+                            changes = true;
+                        } else if (chars[0][i - 1] != '.' && chars[0][i] == '.') {
+                            chars[0][i] = chars[0][i - 1] == '/' ? '/' : '\\';
+                            changes = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        // what next?
+
+        // collect to string
         StringBuilder sb = new StringBuilder();
         for (char[] aChar : chars) {
             for (char c : aChar) {
